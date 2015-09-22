bac0af01f075e7b6466b66266786d187527877df
Nuno Correia <nuno.correia@yourgolftravel.com>
2015-09-22 15:01:48 +0100

    [dev/compile] Better arguments to path.resolve

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,12 @@
 var path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname + "/../src/client/index.js"),
+  entry: path.resolve(__dirname, "..", "src/client/index.js"),
 
   loaders: [{ test: /\.js$/, loader: "babel" }],
 
   output: {
-    path: path.resolve(__dirname + "/../build"),
+    path: path.resolve(__dirname, "..", "build"),
     filename: "bundle.js",
   },
 }
